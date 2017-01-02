fcc30064efcded56b9d985d9ab326ebe3e26fb4c
Francesco Agnoletto <Francesco.agnoletto92@gmail.com>
2017-01-02 22:25:35 +0100

    new session secret

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ mongoose.connect(process.env.MONGO_DATABASE);
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(require('express-session')({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
 
 // Initialize Passport and restore authentication state, if any, from the
 // session.
@@ -88,9 +88,9 @@ app.get('/login/twitter/return',
     res.redirect('/profile');
   });
 
-  app.get('/index', function(req, res){
-    res.redirect('/');
-  })
+app.get('/index', function(req, res){
+  res.redirect('/');
+})
 
 app.get('/profile',
   require('connect-ensure-login').ensureLoggedIn(),
