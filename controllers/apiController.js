cbfc9cc622a1a0af7bd98549600c5684ce5d9ed0
Francesco Agnoletto <Francesco.agnoletto92@gmail.com>
2017-01-04 19:57:19 +0100

    check input validity test

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -7,19 +7,19 @@ module.exports = function(app) {
     app.use(bodyParser.urlencoded({ extended: true }));
 
     app.post('/profile', function(req, res){
-        if(!req.body || req.body.length === 0) {
-            console.log('request body not found');
-            return res.sendStatus(400);
-        }
         var newImage = Image({
                username: req.user.username,
                title: req.body.title,
                imageLink: req.body.imageLink,
                likes: 0
            });
-           newImage.save(function(err) {
-               if (err) throw err;
-               res.redirect('/profile');
-           });
+        newImage.save(function(err) {
+            if (err) throw err;
+            if(!req.body || req.body.length === 0) {
+                console.log('request body not found');
+                return res.sendStatus(400);
+            }
+            res.redirect('/profile');
+        });
     })
 };
