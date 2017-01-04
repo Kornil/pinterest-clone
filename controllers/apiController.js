bacedb8d3a883cc2e828d99aaf39ce22513f4b2c
Francesco Agnoletto <Francesco.agnoletto92@gmail.com>
2017-01-04 17:01:35 +0100

    check input validity test

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -7,9 +7,9 @@ module.exports = function(app) {
     app.use(bodyParser.urlencoded({ extended: true }));
 
     app.post('/profile', function(req, res){
-        if (typeof req.body.title === 'string' ||
+        /*if (typeof req.body.title === 'string' ||
         req.body.title instanceof String &&
-        req.body.imageLink.match(/\.(jpeg|jpg|gif|png)$/) != null){
+        req.body.imageLink.match(/\.(jpeg|jpg|gif|png)$/) != null){*/
         var newImage = Image({
                username: req.user.username,
                title: req.body.title,
@@ -20,8 +20,8 @@ module.exports = function(app) {
                if (err) throw err;
                res.redirect('/profile');
            });
-        }else{
+        /*}else{
             res.redirect('/profile');
-        }
+        }*/
     })
 };
