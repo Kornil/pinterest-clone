cbe587ef6ac262d6e064a39007e568fd5ab5898e
kornil <francesco.agnoletto92@gmail.com>
2017-01-06 21:43:56 +0100

    send delete request from button

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -24,7 +24,7 @@ module.exports = function(app) {
     app.delete('/profile', function(req, res){
         Image.findByIdAndRemove(req.body.id, function(err){
             if (err) throw err;
-            res.send('yay');
+            res.redirect('/profile');
         });
     });
 };
