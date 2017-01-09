7616ee87e0cbef9ec80ad2d3de19dc7830f57613
kornil <francesco.agnoletto92@gmail.com>
2017-01-09 01:19:33 +0100

    add share button test

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -26,4 +26,20 @@ module.exports = function(app) {
             res.redirect('/profile');
         });
     });
+
+    app.post('/profile/retweet', function(req, res){
+        Image.findById(req.body.id, function(err, originalImage){
+            if (err) throw err;
+            var retweet = Image({
+                username: req.user.username,
+                title: originalImage.title,
+                imageLink: originalImage.imageLink,
+                likes: 0
+            });
+            retweet.save(function(err) {
+                if (err) throw err;
+                res.redirect('/profile');
+            });
+        });
+    });
 };
